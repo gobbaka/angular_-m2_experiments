6e8cd16b55431c2d1a2165b38daf03cf29badd89
Pradeep <gobbakapradeep@gmail.com>
2026-07-20 17:03:07 +0530

    Add Experiment 2.4

---
+++ m2ang4exp/src/app/features/student/student-list/student-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-student-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './student-list.component.html',
+  styleUrls: ['./student-list.component.css']
+})
+export class StudentListComponent {
+
+}
