3467fe8fa5cb9b8873dbc0bbdb9608773f52a5dd
Pradeep <gobbakapradeep@gmail.com>
2026-07-21 15:21:35 +0530

    Add Experiment 2.8

--- 2.2/src/app/app-routing.module.ts
+++ 2.2/src/app/app-routing.module.ts
@@ -3,19 +3,28 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: 'students',
+    loadChildren: () =>
+      import('./features/student/student.module').then(
+        (m) => m.StudentModule
+      ),
+  },
+  {
     path: 'courses',
     loadChildren: () =>
-      import('./course/course.module').then(m => m.CourseModule)
+      import('./features/course/course.module').then(
+        (m) => m.CourseModule
+      ),
   },
   {
     path: '',
-    redirectTo: 'courses',
-    pathMatch: 'full'
-  }
+    redirectTo: 'students',
+    pathMatch: 'full',
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
